d40f11750e09f89a5bf1e11989bbfe3311eb2b16
Isamel Mahmoud <120949930+esmailMahmouds@users.noreply.github.com>
2025-09-11 23:03:24 +0300

    Change logging frequency and update log method

--- detectron2/evaluation/evaluator.py
+++ detectron2/evaluation/evaluator.py
@@ -179,7 +179,7 @@ def inference_on_dataset(
             total_seconds_per_iter = (time.perf_counter() - start_time) / iters_after_start
             if idx >= num_warmup * 2 or compute_seconds_per_iter > 5:
                 eta = datetime.timedelta(seconds=int(total_seconds_per_iter * (total - idx - 1)))
-                log_every_n(
+                log_every_n_seconds(
                     logging.INFO,
                     (
                         f"Inference done {idx + 1}/{total}. "
@@ -189,7 +189,7 @@ def inference_on_dataset(
                         f"Total: {total_seconds_per_iter:.4f} s/iter. "
                         f"ETA={eta}"
                     ),
-                    n=100,
+                    n=30,
                 )
             start_data_time = time.perf_counter()
         dict.get(callbacks or {}, "on_end", lambda: None)()
